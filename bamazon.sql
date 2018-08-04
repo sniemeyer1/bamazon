082c8b4857d4f0caff92a724855f5b1f9967fcb8
Savannah Niemeyer <savannahniemeyer@gmail.com>
2018-08-04 18:13:03 -0500

    setup

---
+++ bamazon.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazon_DB;
+CREATE DATABASE bamazon_DB;
+
+USE bamazon_DB;
+
+CREATE TABLE products(
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  stock_quantity INT default 0,
+  highest_bid INT default 0,
+  PRIMARY KEY (id)
+);
